1e12099d7b1df0392b1d2a14a4a4d7ce120305aa
Danii1314 <d.silva.andres73@hotmail.com>
2024-10-02 20:02:34 -0500

    ooooo

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+ int numero=0;
+ int numero1=2;
+        System.out.println("hola que tal como le va ");
+        int suma=numero1+numero;
+        System.out.println("--------"+suma);
     }
 }
